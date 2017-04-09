cea139530902200be03da42c21696b5049603ed5
TheCez <achodankar28@gmail.com>
2017-04-09 21:46:09 +0000

    Bringup for omni

--- omni_titan.mk
+++ omni_titan.mk
@@ -1,9 +1,10 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
 
 PRODUCT_RELEASE_NAME := titan
-PRODUCT_NAME := lineage_titan
+PRODUCT_NAME := omni_titan
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
